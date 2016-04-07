1eb55b1e3ead2340c87d4f5c4052e3a3cca99769
melanie <melanie.l.w.barlow@gmail.com>
2016-04-07 15:13:46 +1000

    Python for retrieving metadata for all products that are available from ABS.Stat

---
+++ python/ABS.Stat_AllProductMetadata_Python3.4.py
@@ -0,0 +1,119 @@
+import logging
+import Queries
+from suds.client import Client
+from suds import WebFault
+from suds import byte_str_class
+import lxml.etree as ET
+import os
+import sys,traceback
+import shlex, subprocess
+
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger('suds.client').setLevel(logging.DEBUG)
+logging.getLogger('suds.transport').setLevel(logging.DEBUG)
+logging.getLogger('suds.xsd.schema').setLevel(logging.DEBUG)
+logging.getLogger('suds.wsdl').setLevel(logging.DEBUG)
+
+url = "http://stat.abs.gov.au/sdmxws/sdmx.asmx?WSDL"
+client = Client(url, retxml=True)
+#print(client)
+
+try:
+    #result = client.service.GetDataStructureDefinition(__inject={'msg': Queries.messageGetDataStructureDefinitionPerProduct})
+
+    assert(Queries.messageGetDataStructureDefinitionAllProducts is not None)
+    print(Queries.messageGetDataStructureDefinitionAllProducts)
+    print("Let's go...")
+    message = byte_str_class(Queries.messageGetDataStructureDefinitionAllProducts, 'utf-8')
+
+    rawResultGetDataStructure = client.service.GetDataStructureDefinition(__inject={'msg': message}, retxml=True)
+
+    print(rawResultGetDataStructure)
+    rootGetDataStructureDefinition_result = ET.fromstring(rawResultGetDataStructure)
+
+    print("Back again...")
+    print(ET.tostring(rootGetDataStructureDefinition_result))
+
+    ET.register_namespace('xml', 'http://www.w3.org/XML/1998/namespace')
+
+    nodeList_KeyFamily = rootGetDataStructureDefinition_result.findall('.//{http://www.SDMX.org/resources/SDMXML/schemas/v2_0/structure}KeyFamily')
+    dictAllProducts = {}
+    for nodeKeyFamily in nodeList_KeyFamily:
+        id = nodeKeyFamily.get('id')
+        print('id: ', id)
+        nodeList_EnglishName = nodeKeyFamily.findall(".//{http://www.SDMX.org/resources/SDMXML/schemas/v2_0/structure}Name")
+        if (len(id) > 0) and (len(nodeList_EnglishName) > 0):
+            print('name: ', nodeList_EnglishName[0].text)
+            dictAllProducts[id] = nodeList_EnglishName[0].text
+
+    allRifCsXml = open('/home/anucsiro/workspace/scripts-as-required/shell/ForABS/RifCs/ABS_AllRifCs.xml', 'w')
+
+    allProductsCsv = open('/home/anucsiro/workspace/scripts-as-required/shell/ForABS/AllProducts.csv', 'w')
+
+    for k, v in iter(dictAllProducts.items()):
+
+        try:
+            print("dictAllProducts[{0}] = {1}".format(k, v))
+            allProductsCsv.write("{0}|{1}\r\n".format(k, v))
+
+            rawResultGetReferenceMetadata = client.service.GetDatasetMetadata(__inject={'msg': byte_str_class(Queries.messageGetDatasetMetadata_productID.replace('productId', k), 'utf-8')}, retxml=True)
+            assert(rawResultGetReferenceMetadata is not None)
+            rootGetReferenceMetadata_result = ET.fromstring(rawResultGetReferenceMetadata)
+            assert(rootGetReferenceMetadata_result is not None)
+            print(ET.tostring(rootGetReferenceMetadata_result, pretty_print=True))
+
+            sdmxXmlFilename = str.format('/home/anucsiro/workspace/scripts-as-required/shell/ForABS/SDMX/%s_SDMX.xml' % k)
+            sdmxXmlFile = open(sdmxXmlFilename, 'w')
+
+            treeGetReferenceMetadata = ET.ElementTree(rootGetReferenceMetadata_result)
+            assert(treeGetReferenceMetadata is not None)
+            print(ET.tostring(treeGetReferenceMetadata, pretty_print=True))
+            sdmxXmlFile.write(ET.tostring(rootGetReferenceMetadata_result, encoding="unicode", method="xml", pretty_print=True))
+            sdmxXmlFile.flush()
+            assert(sdmxXmlFile.__sizeof__() > 0)
+
+            rootTestResult = ET.parse(sdmxXmlFilename)
+            sdmxXmlFile.close()
+
+
+            rifcsXmlFilename = str.format('/home/anucsiro/workspace/scripts-as-required/shell/ForABS/RifCs/%s_RifCs.xml' % k)
+            print(rifcsXmlFilename)
+
+            args = str("%s /home/anucsiro/workspace/scripts-as-required/XSLT/SDMX_To_RIFCS/ABS/abs_sdmx_to_rif-cs.xsl -o:%s" % (sdmxXmlFilename, rifcsXmlFilename))
+            command = str("java -jar /home/anucsiro/workspace/scripts-as-required/XSLT/saxon9he.jar %s /home/anucsiro/workspace/scripts-as-required/XSLT/SDMX_To_RIFCS/ABS/abs_sdmx_to_rif-cs.xsl -o:%s" % (sdmxXmlFilename, rifcsXmlFilename))
+            print(args)
+            print(command)
+            process = subprocess.check_output(["java", "-jar", "/home/anucsiro/workspace/scripts-as-required/XSLT/saxon9he.jar", str.format("%s" % sdmxXmlFilename), "/home/anucsiro/workspace/scripts-as-required/XSLT/SDMX_To_RIFCS/ABS/abs_sdmx_to_rif-cs.xsl", str.format("-o:%s" % rifcsXmlFilename), str.format("global_productName=%s" % v)])
+
+            rootRifCsTestResult = ET.parse(rifcsXmlFilename)
+
+            allRifCsXml.write(ET.tostring(rootRifCsTestResult, encoding="unicode", method="xml", pretty_print=True))
+            allRifCsXml.flush()
+        except Exception as e:
+            print("*** Caught Exception in loop ***")
+            print("-"*60)
+            traceback.print_exc(file=sys.stdout)
+            print("-"*60)
+
+    allRifCsXml.close()
+    allProductsCsv.close()
+
+except WebFault as f:
+    print("*** Caught WebFault ***")
+    print(f.fault)
+    allRifCsXml.close()
+    allProductsCsv.close()
+except Exception as e:
+    print("*** Caught Exception ***")
+    print("-"*60)
+    traceback.print_exc(file=sys.stdout)
+    print("-"*60)
+    allRifCsXml.close()
+    allProductsCsv.close()
+
+
+
+
+
+
+
---
+++ python/Queries.py
@@ -0,0 +1,241 @@
+messageGetDataStructureDefinitionPerProduct = \
+"""<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <soap:Body>
+        <GetDataStructureDefinition xmlns="http://stats.oecd.org/OECDStatWS/SDMX/">
+            <QueryMessage>
+                <message:QueryMessage xmlns="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/query" xmlns:message="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message" xsi:schemaLocation="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/query http://www.sdmx.org/docs/2_0/SDMXQuery.xsd http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message http://www.sdmx.org/docs/2_0/SDMXMessage.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    <message:Header>
+                        <message:ID>none</message:ID>
+                        <message:Test>true</message:Test>
+                        <message:Prepared>2016-04-05T03:57:40</message:Prepared>
+                        <message:Sender id="ABS" />
+                        <message:Receiver id="ABS" />
+                    </message:Header>
+                    <message:Query>
+                        <KeyFamilyWhere>
+                            <Or>
+                                <KeyFamily>CPI</KeyFamily>
+                            </Or>
+                        </KeyFamilyWhere>
+                    </message:Query>
+                </message:QueryMessage>
+            </QueryMessage>
+        </GetDataStructureDefinition>
+    </soap:Body>
+</soap:Envelope>
+"""
+
+messageGetDataStructureDefinitionAllProducts = \
+"""<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <soap:Body>
+        <GetDataStructureDefinition xmlns="http://stats.oecd.org/OECDStatWS/SDMX/">
+            <QueryMessage>
+                <message:QueryMessage xmlns="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/query" xmlns:message="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message" xsi:schemaLocation="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/query http://www.sdmx.org/docs/2_0/SDMXQuery.xsd http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message http://www.sdmx.org/docs/2_0/SDMXMessage.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    <message:Header>
+                        <message:ID>none</message:ID>
+                        <message:Test>false</message:Test>
+                        <message:Prepared>2016-04-05T03:57:40</message:Prepared>
+                        <message:Sender id="ABS" />
+                        <message:Receiver id="ABS" />
+                    </message:Header>
+                    <message:Query>
+                        <KeyFamilyWhere>
+                            <Or>
+                            </Or>
+                        </KeyFamilyWhere>
+                    </message:Query>
+                </message:QueryMessage>
+            </QueryMessage>
+        </GetDataStructureDefinition>
+    </soap:Body>
+</soap:Envelope>
+"""
+
+messageGetDataStructureDefinition_CPI = \
+"""<?xml version="1.0" encoding="UTF-8"?>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <soap:Body>
+        <GetDataStructureDefinition xmlns="http://stats.oecd.org/OECDStatWS/SDMX/">
+            <QueryMessage>
+                <message:QueryMessage xmlns="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/query" xmlns:message="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message" xsi:schemaLocation="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/query http://www.sdmx.org/docs/2_0/SDMXQuery.xsd
+                    http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message http://www.sdmx.org/docs/2_0/SDMXMessage.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    <Header xmlns="http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message">
+                        <ID>none</ID>
+                        <Test>false</Test>
+                        <Prepared>2012-06-01T09:33:53</Prepared>
+                        <Sender id="YourID">
+                            <Name xml:lang="en">Your English Name</Name>
+                        </Sender>
+                        <Receiver id="ABS">
+                            <Name xml:lang="en">Australian Bureau of Statistics</Name>
+                            <Name xml:lang="fr">Australian Bureau of Statistics</Name>
+                        </Receiver>
+                    </Header>
+                    <message:Query>
+                        <KeyFamilyWhere>
+                            <Or>
+                                <KeyFamily>CPI</KeyFamily>
+                            </Or>
+                        </KeyFamilyWhere>
+                    </message:Query>
+                </message:QueryMessage>
+            </QueryMessage>
+        </GetDataStructureDefinition>
+    </soap:Body>
+</soap:Envelope>"""
+
+messageGetMetadata_productID = \
+"""<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"    xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <soap:Body>
+        <GetMetadata xmlns="http://stats.oecd.org/OECDStatWS/SDMX/">
+            <QueryMessage>
+                <message:GenericMetadataQuery
+                    xmlns:message="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/message"
+                    xmlns:structure="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/structure"
+                    xmlns:query="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/query"
+                    xmlns:common="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/common"
+                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="
+                    http://www.sdmx.org/resources/sdmxml/schemas/v2_1/query SDMXQueryMetadata.xsd
+                    http://www.sdmx.org/resources/sdmxml/schemas/v2_1/message SDMXMessage.xsd">
+                    <message:Header>
+                        <message:ID>none</message:ID>
+                        <message:Test>false</message:Test>
+                        <message:Prepared>2016-04-05T03:57:40</message:Prepared>
+                        <message:Sender id="ABS" />
+                        <message:Receiver id="ABS" />
+                    </message:Header>
+                    <message:Query>
+                        <query:ReturnDetails/>
+                        <query:MetadataParameters>
+                            <query:AttachedDataSet>
+                                <common:DataProvider>
+                                    <Ref id="ABS" maintainableParentID="" agencyID="ABS"/>
+                                </common:DataProvider>
+                                <common:ID>productId</common:ID>
+                            </query:AttachedDataSet>
+                        </query:MetadataParameters>
+                    </message:Query>
+                </message:GenericMetadataQuery>
+            </QueryMessage>
+        </GetDatasetMetadata>
+    </soap:Body>
+</soap:Envelope>
+"""
+
+messageGetReferenceMetadata_productID = \
+"""<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"    xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <soap:Body>
+        <GetReferenceMetadata xmlns="http://stats.oecd.org/OECDStatWS/SDMX/">
+            <XmlQuery>
+                <message:GenericMetadataQuery
+                    xmlns:message="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/message"
+                    xmlns:structure="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/structure"
+                    xmlns:query="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/query"
+                    xmlns:common="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/common"
+                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="
+                    http://www.sdmx.org/resources/sdmxml/schemas/v2_1/query SDMXQueryMetadata.xsd
+                    http://www.sdmx.org/resources/sdmxml/schemas/v2_1/message SDMXMessage.xsd">
+                    <message:Header>
+                        <message:ID>none</message:ID>
+                        <message:Test>false</message:Test>
+                        <message:Prepared>2016-04-05T03:57:40</message:Prepared>
+                        <message:Sender id="ABS" />
+                        <message:Receiver id="ABS" />
+                    </message:Header>
+                    <message:Query>
+                        <query:ReturnDetails/>
+                        <query:MetadataParameters>
+                            <query:AttachedDataSet>
+                                <common:DataProvider>
+                                    <Ref id="ABS" maintainableParentID="" agencyID="ABS"/>
+                                </common:DataProvider>
+                                <common:ID>productId</common:ID>
+                            </query:AttachedDataSet>
+                        </query:MetadataParameters>
+                    </message:Query>
+                </message:GenericMetadataQuery>
+            </XmlQuery>
+        </GetReferenceMetadata>
+    </soap:Body>
+</soap:Envelope>
+"""
+
+messageGetDatasetMetadata_productID = \
+"""<?xml version="1.0" encoding="UTF-8"?>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"    xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <soap:Body>
+        <GetDatasetMetadata xmlns="http://stats.oecd.org/OECDStatWS/SDMX/">
+            <QueryMessage>
+                <message:GenericMetadataQuery
+xmlns:message="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/message"
+xmlns:structure="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/structure"
+xmlns:query="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/query"
+xmlns:common="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/common"
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="
+http://www.sdmx.org/resources/sdmxml/schemas/v2_1/query SDMXQueryMetadata.xsd
+http://www.sdmx.org/resources/sdmxml/schemas/v2_1/message SDMXMessage.xsd">
+                    <message:Header>
+                        <message:ID>none</message:ID>
+                        <message:Test>false</message:Test>
+                        <message:Prepared>2012-10-17T03:57:40</message:Prepared>
+                        <message:Sender id="ABS" />
+                        <message:Receiver id="ABS" />
+                    </message:Header>
+                    <message:Query>
+                        <query:ReturnDetails/>
+                        <query:MetadataParameters>
+                            <query:AttachedDataSet>
+                                <common:DataProvider>
+                                    <Ref id="ABS" maintainableParentID="" agencyID="ABS"/>
+                                </common:DataProvider>
+                                <common:ID>productId</common:ID>
+                            </query:AttachedDataSet>
+                        </query:MetadataParameters>
+                    </message:Query>
+                </message:GenericMetadataQuery>
+            </QueryMessage>
+        </GetDatasetMetadata>
+    </soap:Body>
+</soap:Envelope>
+"""
+
+messageGetMetadata_CPI_FromLiam = \
+"""<?xml version="1.0" encoding="UTF-8"?>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"    xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <soap:Body>
+        <GetDatasetMetadata xmlns="http://stats.oecd.org/OECDStatWS/SDMX/">
+            <QueryMessage>
+                <message:GenericMetadataQuery
+xmlns:message="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/message"
+xmlns:structure="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/structure"
+xmlns:query="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/query"
+xmlns:common="http://www.sdmx.org/resources/sdmxml/schemas/v2_1/common"
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="
+http://www.sdmx.org/resources/sdmxml/schemas/v2_1/query SDMXQueryMetadata.xsd
+http://www.sdmx.org/resources/sdmxml/schemas/v2_1/message SDMXMessage.xsd">
+                    <message:Header>
+                        <message:ID>none</message:ID>
+                        <message:Test>false</message:Test>
+                        <message:Prepared>2012-10-17T03:57:40</message:Prepared>
+                        <message:Sender id="ABS" />
+                        <message:Receiver id="ABS" />
+                    </message:Header>
+                    <message:Query>
+                        <query:ReturnDetails/>
+                        <query:MetadataParameters>
+                            <query:AttachedDataSet>
+                                <common:DataProvider>
+                                    <Ref id="ABS" maintainableParentID="" agencyID="ABS"/>
+                                </common:DataProvider>
+                                <common:ID>CPI</common:ID>
+                            </query:AttachedDataSet>
+                        </query:MetadataParameters>
+                    </message:Query>
+                </message:GenericMetadataQuery>
+            </QueryMessage>
+        </GetDatasetMetadata>
+    </soap:Body>
+</soap:Envelope>
+"""
