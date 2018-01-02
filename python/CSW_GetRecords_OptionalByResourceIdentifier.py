220766c99e31bda831cbdee20f0cce7ae3c5784e
manyeggsinonebasket@gmail.com <melanie.l.w.barlow@gmail.com>
2018-01-03 10:28:31 +1100

    File for calling GetRecords over CSW, with optional querying by resource identifier

---
+++ python/CSW_GetRecords_OptionalByResourceIdentifier.py
@@ -0,0 +1,45 @@
+import urllib2
+import sys
+import re
+import Test_OGC_CSW_Functions
+
+
+# Call like so for example, querying by certain ResourceIdentifier:
+# http://test.domain.org/csw_endpoint identifier1,identifier2,identifier3
+
+def main():
+
+    print(len(sys.argv))
+    assert(sys.argv[1] != None)
+
+    print(sys.argv[1])
+
+    url = sys.argv[1]
+
+    paramList = list()
+
+    conditionDict = None;
+
+    if (len(sys.argv) > 2):
+        print(sys.argv[2])
+
+        identifierList = sys.argv[2].split(',')
+
+        assert((identifierList.__class__.__name__) == 'list')
+
+
+
+        for identifier in identifierList:
+            print(identifier)
+
+            paramDict = dict({"name" : "ResourceIdentifier", "value" : identifier})
+            paramList.append(paramDict)
+
+        conditionDict = dict({"Or" : paramList})
+
+    Test_OGC_CSW_Functions.callCSW(url, conditionDict)
+
+
+if __name__ == "__main__":
+    main()
+
